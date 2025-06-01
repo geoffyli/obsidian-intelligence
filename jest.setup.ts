33b590036f40ee958cea46dc90ce765042f94ec3
geoffyli <geoff.yulong.li@gmail.com>
2025-06-01 17:59:41 +0800

    Adopted React for front-end dev.

--- jest.setup.ts
+++
@@ -1,4 +0,0 @@
-import '@testing-library/jest-dom';   // if you ever add DOM tests
-import 'jest-extended';              // richer matchers
-
-jest.setTimeout(15_000);             // generous async limit
